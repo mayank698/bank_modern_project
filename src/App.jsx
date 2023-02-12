fc1cec62afc19b8d7a1bce123ea7b3dd325b4f83
mayank698 <83001650+mayank698@users.noreply.github.com>
2023-02-12 16:50:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./styles";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Business from "./components/Business";
+import Billing from "./components/Billing";
+import CardDeal from "./components/CardDeal";
+import Testimonials from "./components/Testimonials";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}><Navbar/></div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
